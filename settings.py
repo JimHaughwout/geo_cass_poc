cf0853dc5aa7dc4d21e35a0abba94c0f2356913c
Jim Haughwout <jim.haughwout@gmail.com>
2014-11-05 15:20:26 -0500

    Changed Cassandra Key Space. Cleanup

--- settings.py
+++ settings.py
@@ -1,15 +1,15 @@
 from models import Bounding_Box
 
 # Global Variables
-KEY_SPACE = 'test' # Cassandra Keyspace
+KEY_SPACE = 'dm' # Cassandra Keyspace
 ORG = 'SGS'
-START_DT = '2014-11-02 23:15:15'
+START_DT = '2014-11-05 17:00:00'
 SPEED = 40 # kph
 SPEED_VARIANCE = 10.00 # N% variance between reads
 REPORTING_INTERVAL = 900 # 900 seconds between reads
-THING_COUNT = 10000
+THING_COUNT = 1000
 TIME_VARIANCE = 5.00 # N% variance between time invervals
-CYCLE_COUNT = 1
+CYCLE_COUNT = 15
 BOUNDING_BOXES = ( # Bounding boxes in UL, LR geo format
     Bounding_Box((14, -4), (6, 8)),
     Bounding_Box((1.4, 31), (0, 36)),
